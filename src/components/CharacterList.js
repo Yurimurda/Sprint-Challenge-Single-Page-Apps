b1a4a1f0b0ef64b38be4bcdde8d7549db6c9a8f0
Yurimurda <lolsgotpwnd@yahoo.com>
2019-10-18 19:24:04 -0500

    MVP met

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,104 +1,28 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import SearchForm from "./SearchForm";
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [data, setData] = useState([]);
-  const [query, setQuery] = useState("");
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
       .get(`https://rickandmortyapi.com/api/character/`)
         .then(response =>{
-          const characters = response.data.results.filter(character =>
-            character.name.toLowerCase().includes(query.toLowerCase())
-          );
+          
           console.log(response);
-          setData(characters);
+          setData(response.data.results);
         })
         .catch(error =>{
           console.log(error);
         });
-      }, [query]);
-  const handleInputChange = event => {
-    setQuery(event.target.value);
-  };
-  
-  return (
-    <div className="Characters">
-      <form className="search">
-        <input
-          type="text"
-          onChange={handleInputChange}
-          value={query}
-          name="name"
-          tabIndex="0"
-          className="prompt search-name"
-          placeholder="Search by name"
-          autoComplete="off"
-        />
-      </form>
-      <div className="Character">
-        {data.map(data => {
-          return (
-            <div className="character-list " key={data.id}>
-              <h2>
-                {data.name}
-              </h2>
-              <h3 className="status">status: {data.status}</h3>
-              <h3 className="species">species: {data.species}</h3>
-              <h3 className="gender">gender: {data.gender}</h3>
-              <img src={data.image} alt={data.name}/>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+      }, []);
+  return( 
+    <div><SearchForm data={data}/></div> 
+  )
 }
 
 
 
 
-// export default function Characters() {
-  
-//   useEffect(() => {
-    
-//   }, [query]);
-//   const handleInputChange = event => {
-//     setQuery(event.target.value);
-//   };
-//   return (
-//     <div className="Characters">
-//       <form className="search">
-//         <input
-//           type="text"
-//           onChange={handleInputChange}
-//           value={query}
-//           name="name"
-//           tabIndex="0"
-//           className="prompt search-name"
-//           placeholder="search by name"
-//           autoComplete="off"
-//         />
-//       </form>
-//       <div className="Character">
-//         {data.map(data => {
-//           return (
-//             <div className="character-list " key={data._id}>
-//               <h2>
-//                 {data.name}
-//               </h2>
-//               <h3 className="capital">name: {data.name}</h3>
-//               <h3 className="capital">status: {data.status}</h3>
-//               <h3 className="capital">species: {data.species}</h3>
-//               <h3 className="capital">type: {data.type}</h3>
-//               <h3 className="capital">gender: {data.gender}</h3>
-//               <h3 className="insert">origin:{data.origin}</h3>
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </div>
-//   );
-// 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,47 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
+const SearchForm=(props)=> {
+  const [query, setQuery] = useState("");
+  
+  const characters = props.data.filter(character =>
+    character.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  };
+  
   return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
+    <div className="Characters">
+      <form className="search">
+        <input
+          type="text"
+          onChange={handleInputChange}
+          value={query}
+          name="name"
+          tabIndex="0"
+          className="prompt search-name"
+          placeholder="Search by name"
+          autoComplete="off"
+        />
+      </form>
+      <div className="Character">
+        {characters.map(data => {
+          return (
+            <div className="character-list " key={data.id}>
+              <h2>
+                {data.name}
+              </h2>
+              <h3 className="status">status: {data.status}</h3>
+              <h3 className="species">species: {data.species}</h3>
+              <h3 className="gender">gender: {data.gender}</h3>
+              <img src={data.image} alt={data.name}/>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
+
+export default SearchForm;
